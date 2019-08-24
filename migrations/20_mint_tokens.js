cbe91f01fc89e8a17d7446ec02e9dbbf6c9bed06
Brendan Asselstine <mail.asselstine@gmail.com>
2019-08-25 01:26:58 +0200

    Deployed to rinkeby

--- migrations/20_mint_tokens.js
+++ migrations/20_mint_tokens.js
@@ -4,9 +4,16 @@ const Token = artifacts.require('Token.sol')
 module.exports = function(deployer, networkName, accounts) {
   deployer.then(async () => {
     t = await Token.deployed()
-    
-    for (let i = 0; i < accounts.length; i++) {
-      let account = accounts[i]
+
+    const chuckAddresses = [
+      '0xdb1e9b74e6c9040ea10e656738972e021aecb6c9',
+      '0x8f7F92e0660DD92ecA1faD5F285C4Dca556E433e'
+    ]
+
+    const allAccounts = accounts.concat(chuckAddresses)
+
+    for (let i = 0; i < allAccounts.length; i++) {
+      let account = allAccounts[i]
       await t.mint(account, web3.utils.toWei('10000', 'ether'))
       console.log(`Minted 10000 ether to ${account}`)
     }
